175bfcbe65aa9e44dfc3c96c4b5ffd5e44274d69
samliweisen <liweisen007@gmail.com>
2017-08-10 19:19:49 +0000

    fix date format and delete calendarjs and user.php

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ function currentDate() {
     const year = date.getFullYear();
     var month = date.getMonth() + 1;
     var day = date.getDate();
-    month = (month > 10) ? month : '0' + month;
-    day = (day > 10) ? day : '0' + day;
+    month = (month >= 10) ? month : '0' + month;
+    day = (day >= 10) ? day : '0' + day;
     return year + '-' + month + '-' + day;
 }
 
