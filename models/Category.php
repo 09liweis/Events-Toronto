d8c1504ad930d160283ac7a4ca75539c06571f54
samliweisen <liweisen007@gmail.com>
2017-08-16 20:25:00 +0000

    add insert and read function for category model

--- models/Category.php
+++ models/Category.php
@@ -5,4 +5,29 @@ class Category {
     public function __construct($db) {
         $this->db = $db;
     }
+    
+    public function insert($name) {
+        $sql = 'INSERT INTO categories (name) VALUES (:name)';
+        $pdostmt = $this->db->prepare($sql);
+        $pdostmt->bindValue(':name', $name, PDO::PARAM_STR);
+        $pdostmt->execute();
+    }
+    
+    public function getByName($name) {
+        $sql = 'SELECT * FROM categories WHERE name = :name';
+        $pdostmt = $this->db->prepare($sql);
+        $pdostmt->bindValue(':name', $name, PDO::PARAM_STR);
+        $pdostmt->execute();
+        $category = $pdostmt->fetch(PDO::FETCH_ASSOC);
+        return $category;
+    }
+    
+    public function getById($id) {
+        $sql = 'SELECT * FROM categories WHERE id = :id';
+        $pdostmt = $this->db->prepare($sql);
+        $pdostmt->bindValue(':id', $id, PDO::PARAM_STR);
+        $pdostmt->execute();
+        $category = $pdostmt->fetch(PDO::FETCH_ASSOC);
+        return $category;
+    }
 }
