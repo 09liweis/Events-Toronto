f367ce47bc33d72969d4184124690e6a39f8235a
samliweisen <liweisen007@gmail.com>
2018-05-05 03:49:57 +0000

    add name, address, location to events

--- database/migrations/2018_05_05_034650_events.php
+++ database/migrations/2018_05_05_034650_events.php
@@ -13,7 +13,13 @@ class Events extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('address');
+            $table->string('location');
+            $table->timestamps();
+        });
     }
 
     /**
