a6de68f688d65565d392cc980fec2bfd467fe6e9
samliweisen <liweisen007@gmail.com>
2018-05-05 17:05:37 +0000

    remove unuse file

--- cron/importVisuals.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '../models/Database.php';
-require '../models/Visual.php';
-
-$visual = new Visual(Database::dbConnect());
-$url = 'http://whatiwatched-samliweisen.rhcloud.com/visual/visuals_json';
-$data = json_decode(file_get_contents($url), true);
-$visual->insert($data);
--- cron/updateEvents.php
+++
@@ -1,9 +0,0 @@
-<?php
-require '../models/Database.php';
-require '../models/API.php';
-require '../models/Event.php';
-
-$event = new Event(Database::dbConnect());
-$api = new API();
-$data = $api->getData();
-$event->updateEvents($data);
