e59d06e3b741ceaf88b2fe4ae5e2472a6dd079bf
samliweisen <liweisen007@gmail.com>
2018-05-05 04:09:44 +0000

    set up import route and method

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Event;
 
 class EventController extends Controller
 {
-    //
+    public function import() {
+        $api = 'http://app.toronto.ca/cc_sr_v1_app/data/edc_eventcal_APR?limit=1000';
+        $importEvents = json_decode(file_get_contents($api), true);
+        return $importEvents;
+    }
 }
