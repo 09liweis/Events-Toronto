257e3dbeb6ef28e5e32ac54a7fc97d5e20bf8581
samliweisen <liweisen007@gmail.com>
2018-05-06 21:28:22 +0000

    config env

--- config/database.php
+++ config/database.php
@@ -1,5 +1,18 @@
 <?php
 
+if (getenv('APP_ENV') == 'production') {
+    $url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+    $host = $url['host'];
+    $username = $url['user'];
+    $password = $url['pass'];
+    $database = substr($url['path'], 1);
+} else {
+    $host = 'localhost';
+    $username = 'a09liweis';
+    $password = '';
+    $database = 'c9';
+}
+
 return [
 
     /*
@@ -41,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $host,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
