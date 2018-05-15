f2786deb245aee53983dffe963082444694beb51
samliweisen <liweisen007@gmail.com>
2018-05-15 19:55:47 +0000

    remove old template

--- template/footer.php
+++
@@ -1,9 +0,0 @@
-    </div>
-    </body>
-        <script type="text/javascript">
-            var currentLocation = <?=json_encode($currentLocation)?>;
-        </script>
-        <?php foreach ($scripts as $script) { ?>
-        <script type="text/javascript" src="<?=$script?>"></script>
-        <?php } ?>
-</html>
