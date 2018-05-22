04848ecb5954ed5e906ae6b5d9567da910ce9114
samliweisen <liweisen007@gmail.com>
2018-05-22 18:23:38 +0000

    asset version update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,14 +11,14 @@
     <title>{{ config('app.name', 'Events Toronto') }}</title>
 
     <!-- Scripts -->
-    <script src="/js/app.js?v=1.0.2" defer></script>
+    <script src="/js/app.js?v=1.0.3" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="/css/app.css?v=1.0.3" rel="stylesheet">
+    <link href="/css/app.css?v=1.0.4" rel="stylesheet">
 </head>
 <body>
     <div id="app">
