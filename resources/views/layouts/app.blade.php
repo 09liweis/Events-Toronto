72a98fe3b13187f469a2b3b39eabb62c27be886e
samliweisen <liweisen007@gmail.com>
2018-05-22 15:31:33 +0000

    comment out login section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="/css/app.css?v=1.0.2" rel="stylesheet">
+    <link href="/css/app.css?v=1.0.3" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -33,36 +33,36 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    <!--<ul class="navbar-nav mr-auto">-->
 
-                    </ul>
+                    <!--</ul>-->
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <!--<ul class="navbar-nav ml-auto">-->
                         <!-- Authentication Links -->
-                        @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                    <!--    @guest-->
+                    <!--        <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>-->
+                    <!--        <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>-->
+                    <!--    @else-->
+                    <!--        <li class="nav-item dropdown">-->
+                    <!--            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>-->
+                    <!--                {{ Auth::user()->name }} <span class="caret"></span>-->
+                    <!--            </a>-->
 
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                    <!--            <div class="dropdown-menu" aria-labelledby="navbarDropdown">-->
+                    <!--                <a class="dropdown-item" href="{{ route('logout') }}"-->
+                    <!--                   onclick="event.preventDefault();-->
+                    <!--                                 document.getElementById('logout-form').submit();">-->
+                    <!--                    {{ __('Logout') }}-->
+                    <!--                </a>-->
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                    <!--                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">-->
+                    <!--                    @csrf-->
+                    <!--                </form>-->
+                    <!--            </div>-->
+                    <!--        </li>-->
+                    <!--    @endguest-->
+                    <!--</ul>-->
                 </div>
             </div>
         </nav>
