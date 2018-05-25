3003479e37fbb1ae7195823b0321a50baf8f9f89
samliweisen <liweisen007@gmail.com>
2018-05-25 19:11:34 +0000

    add categories list api end point

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/import', 'EventController@import');
 Route::get('/events', 'EventController@list');
 Route::get('/event/{id}', 'EventController@detail');
+
+Route::get('/categories', 'CategoryController@list');
