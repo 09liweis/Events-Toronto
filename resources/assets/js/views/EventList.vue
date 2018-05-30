d615cee9297f25a8fadd1997820349fc99c18c16
samliweisen <liweisen007@gmail.com>
2018-05-30 14:12:06 +0000

    disable mobile datepicker

--- resources/assets/js/views/EventList.vue
+++ resources/assets/js/views/EventList.vue
@@ -37,7 +37,7 @@ export default {
             list: [],
             date: '',
             config: {
-                inline: true
+                disableMobile: true
             }
         };
     },
