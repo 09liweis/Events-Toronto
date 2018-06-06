8a5d216f4aa4ed62638d8c65264685242472f77c
samliweisen <liweisen007@gmail.com>
2018-06-06 13:23:16 +0000

    update asset version

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,14 +14,14 @@
     <meta name="robots" content="index, follow" />
 
     <!-- Scripts -->
-    <script src="/js/app.js?v=1.0.5" defer></script>
+    <script src="/js/app.js?v=1.0.6" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="/css/app.css?v=1.0.7" rel="stylesheet">
+    <link href="/css/app.css?v=1.0.8" rel="stylesheet">
 </head>
 <body>
     <div id="app">
