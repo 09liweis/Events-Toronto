7e45add632d4412650528c1d4e6165dc8f97581c
samliweisen <liweisen007@gmail.com>
2018-06-11 13:51:23 +0000

    re construct event detail component

--- resources/assets/js/views/EventDetail.vue
+++ resources/assets/js/views/EventDetail.vue
@@ -1,27 +1,31 @@
 <template>
-    <div id="detail" class="row" v-if="event">
-        <div class="col-md-4">
-            <img :src="event.image" :alt="event.name" class="img-fluid" />
-            <div class="event__categories"><span class="event__category" v-for="c in event.categories">{{c.name}}</span></div>
+    <div id="detail" v-if="event">
+        <div class="row">
+            <div class="col-md-4">
+                <img :src="event.image" :alt="event.name" class="img-fluid" />
+                <div class="event__categories"><span class="event__category" v-for="c in event.categories">{{c.name}}</span></div>
+            </div>
+            <div class="col-md-8">
+                <h1 v-html="event.name"></h1>
+                <p v-html="event.description"></p>
+                <div>{{event.start_date}} - {{event.end_date}}</div>
+                <div>Free: {{event.free}}</div>
+                <div>Location: {{event.location}}</div>
+                <div>Address: {{event.address}}</div>
+                <a target="_blank" :href="event.website">{{event.website}}</a>
+            </div>
         </div>
-        <div class="col-md-8">
-            <h1 v-html="event.name"></h1>
-            <p v-html="event.description"></p>
-            <div>{{event.start_date}} - {{event.end_date}}</div>
-            <div>Free: {{event.free}}</div>
-            <div>Location: {{event.location}}</div>
-            <div>Address: {{event.address}}</div>
-            <a target="_blank" :href="event.website">{{event.website}}</a>
-        </div>
-        <div class="col-md-12">
-            <gmap-map class="map" :center="position" :zoom="15">
-                <GmapMarker
-                    :position="position"
-                    :clickable="true"
-                    :draggable="true"
-                    @click="center = position"
-                />
-            </gmap-map>
+        <div class="rowm">
+            <div class="col-md-12">
+                <gmap-map class="map" :center="position" :zoom="15">
+                    <GmapMarker
+                        :position="position"
+                        :clickable="true"
+                        :draggable="true"
+                        @click="center = position"
+                    />
+                </gmap-map>
+            </div>
         </div>
     </div>
 </template>
