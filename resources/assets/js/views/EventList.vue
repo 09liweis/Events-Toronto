6a919550ba9c2760177b5f5829d23887d9931222
samliweisen <liweisen007@gmail.com>
2018-06-17 22:45:17 +0000

    replace with thumbnail with image

--- resources/assets/js/views/EventList.vue
+++ resources/assets/js/views/EventList.vue
@@ -26,7 +26,7 @@
             <div class="col-md-8">
                 <div class="event row" v-for="event in events" v-on:click="viewEvent(event.id)" v-bind:class="{ selected: eventId == event.id}">
                     <figure class="col-md-3 event__figure">
-                        <img class="event__thumbnail" :src="event.thumbnail" :alt="event.name">
+                        <img class="event__thumbnail" :src="event.image" :alt="event.name">
                     </figure>
                     <div class="col-md-9 event__info">
                         <!--<router-link :to="{ name: 'detail', params: { id: event.id }}"><h5 class="card-title" v-html="event.name"></h5></router-link>-->
