008a73d94e59d16ee64c76c95539909e148aae63
samliweisen <liweisen007@gmail.com>
2018-06-25 18:19:41 +0000

    call api in dooropen

--- resources/assets/js/views/DoorOpen.vue
+++ resources/assets/js/views/DoorOpen.vue
@@ -1,12 +1,23 @@
 <template>
-    Door Open
+    <div>
+        <h1>Door Open 2018</h1>
+    </div>
 </template>
 <script>
+import axios from 'axios';
 export default {
     data() {
         return {
-            
+            doors: []
         };
+    },
+    mounted() {
+        axios.get('api/dooropen').then(res => {
+            this.doors = res.data;
+        });
+    },
+    methods: {
+        
     }
 };
 </script>
