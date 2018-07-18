6e1f83f59725881ec02a236a36ffac5cf2a515fe
samliweisen <liweisen007@gmail.com>
2018-07-18 15:24:46 +0000

    add loading

--- resources/assets/js/views/DoorOpen.vue
+++ resources/assets/js/views/DoorOpen.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>Door Open 2018</h1>
+        <div class="lds-ellipsis" v-if="doors.length == 0"><div></div><div></div><div></div><div></div></div>
         <div class="row" v-if="view == 'list'">
             <div class="door col-hd-2 col-md-3 col-sm-4" v-for="d in doors">
                 <h3 class="door__title">{{d.dot_buildingName}}</h3>
@@ -64,6 +65,63 @@ export default {
 };
 </script>
 <style type="text/css" scoped>
+.lds-ellipsis {
+  display: inline-block;
+  position: relative;
+  width: 64px;
+  height: 64px;
+}
+.lds-ellipsis div {
+  position: absolute;
+  top: 27px;
+  width: 11px;
+  height: 11px;
+  border-radius: 50%;
+  background: #cef;
+  animation-timing-function: cubic-bezier(0, 1, 1, 0);
+}
+.lds-ellipsis div:nth-child(1) {
+  left: 6px;
+  animation: lds-ellipsis1 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(2) {
+  left: 6px;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(3) {
+  left: 26px;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(4) {
+  left: 45px;
+  animation: lds-ellipsis3 0.6s infinite;
+}
+@keyframes lds-ellipsis1 {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes lds-ellipsis3 {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
+@keyframes lds-ellipsis2 {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(19px, 0);
+  }
+}
+
+
 .map {
     width: 100%;
     height: 80vh;
