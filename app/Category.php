ad545275d74f2eb298b169093c2be5d7cfd5d9d4
samliweisen <liweisen007@gmail.com>
2018-08-01 13:07:17 +0000

    add pivot as hidden field for event category

--- app/Category.php
+++ app/Category.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $hidden = array('pivot');
+    
     public function events() {
         return $this->belongsToMany('App\Event')->select(array(
             'events.id',
