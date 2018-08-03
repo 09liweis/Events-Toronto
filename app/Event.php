c3cd8792c76158dfdb11c620b385144dac615b69
samliweisen <liweisen007@gmail.com>
2018-08-03 19:02:44 +0000

    add coment

--- app/Event.php
+++ app/Event.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    // return category id and name
     public function categories() {
         return $this->belongsToMany('App\Category')->select('categories.id', 'name');
     }
