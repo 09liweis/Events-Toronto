987dee0525eec065f938ed827eb0ecbb44fb92a5
samliweisen <liweisen007@gmail.com>
2018-08-27 13:13:58 +0000

    implement categories component

---
+++ resources/assets/js/components/Categories.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="event__categories"><span class="event__category" v-for="c in categories">{{c.name}}</span></div>
+</template>
+<script>
+export default {
+    props: ['categories']
+};
+</script>
