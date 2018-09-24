7e41b68af5c22192e777642d490f9a4f79111963
samliweisen <liweisen007@gmail.com>
2018-09-24 18:46:25 +0000

    tab in html

--- resources/assets/js/views/EventList.vue
+++ resources/assets/js/views/EventList.vue
@@ -29,19 +29,18 @@
             </div>
             <div class="col-md-8">
                 <transition-group name="slide">
-                <div class="event row" v-for="event in events" :key="event.id" v-on:click="viewEvent(event.id)" v-bind:class="{ selected: eventId == event.id}">
-                    <figure class="col-md-3 event__figure">
-                        <img class="event__thumbnail" :src="event.image" :alt="event.name">
-                    </figure>
-                    <div class="col-md-9 event__info">
-                        <!--<router-link :to="{ name: 'detail', params: { id: event.id }}"><h5 class="card-title" v-html="event.name"></h5></router-link>-->
-                        <h3 v-html="event.name"></h3>
-                        <div><i class="fas fa-calendar-alt"></i>{{event.start_date}}</div>
-                        <div><i class="fas fa-map-marker-alt"></i>{{event.location}}</div>
-                        <div>{{event.address}}</div>
-                        <Categories v-bind:categories="event.categories" />
+                    <div class="event row" v-for="event in events" :key="event.id" v-on:click="viewEvent(event.id)" v-bind:class="{ selected: eventId == event.id}">
+                        <figure class="col-md-3 event__figure">
+                            <img class="event__thumbnail" :src="event.image" :alt="event.name">
+                        </figure>
+                        <div class="col-md-9 event__info">
+                            <h3 v-html="event.name"></h3>
+                            <div><i class="fas fa-calendar-alt"></i>{{event.start_date}}</div>
+                            <div><i class="fas fa-map-marker-alt"></i>{{event.location}}</div>
+                            <div>{{event.address}}</div>
+                            <Categories v-bind:categories="event.categories" />
+                        </div>
                     </div>
-                </div>
                 </transition-group>
             </div>
         </div>
@@ -70,7 +69,7 @@ export default {
     components: {
         flatPickr,
         EventDetail,
-        Categories
+        Categories,
     },
     data() {
         return {
