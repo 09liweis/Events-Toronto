e7759ad6df2712f29b97937fa7536b9ed925e1fd
samliweisen <liweisen007@gmail.com>
2018-10-04 16:05:13 +0000

    fix month issue

--- resources/assets/js/views/EventList.vue
+++ resources/assets/js/views/EventList.vue
@@ -160,7 +160,7 @@ export default {
         },
         formatDate(date) {
             const year = date.getFullYear();
-            const month = date.getMonth() > 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1);
+            const month = date.getMonth() >= 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1);
             const day = date.getDate() > 9 ? date.getDate() : '0' + date.getDate();
             return year + '-' + month + '-' + day;
         },
