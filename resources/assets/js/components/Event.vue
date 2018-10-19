c9c18c8b40d63c7566b922a43f7c30d4d90bc720
samliweisen <liweisen007@gmail.com>
2018-10-19 16:06:04 +0000

    add Event component

---
+++ resources/assets/js/components/Event.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="event row">
+        <figure class="col-md-3 event__figure">
+            <img class="event__thumbnail" :src="event.image" :alt="event.name">
+        </figure>
+        <div class="col-md-9 event__info">
+            <h3 v-html="event.name"></h3>
+            <div><i class="fas fa-calendar-alt"></i>{{event.start_date}}</div>
+            <div><i class="fas fa-map-marker-alt"></i>{{event.location}}</div>
+            <div>{{event.address}}</div>
+            <Categories v-bind:categories="event.categories" />
+        </div>
+    </div>
+</template>
+<script>
+import Categories from './Categories.vue';
+export default {
+    components: {
+        Categories,
+    },
+    props: ['event']
+};
+</script>
