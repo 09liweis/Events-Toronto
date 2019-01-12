6352c880ec771cdeb58f2fb45710756290b8fec6
samliweisen <liweisen007@gmail.com>
2019-01-12 22:57:13 +0000

    add website attribute to get event detail

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -106,7 +106,7 @@ class EventController extends Controller
     }
     
     public function detail(int $id): Event {
-        return Event::select('id', 'name', 'image', 'description', 'start_date', 'end_date', 'location', 'address', 'lat', 'lng')->where('id', $id)->with('categories')->first();
+        return Event::select('id', 'name', 'image', 'description', 'start_date', 'end_date', 'location', 'address', 'lat', 'lng', 'website')->where('id', $id)->with('categories')->first();
     }
     
     public function test(Request $request) {
