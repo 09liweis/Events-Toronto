508548c209717ab43f15a2a93978a3e92f0b3f65
Sam Li <weisen.li@hotmail.com>
2020-03-19 22:14:48 -0400

    fix indentation of Event.php

--- app/Event.php
+++ app/Event.php
@@ -4,10 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Event extends Model
-{
-    // return category id and name
-    public function categories() {
-        return $this->belongsToMany('App\Category')->select('categories.id', 'name');
-    }
+class Event extends Model {
+	// return category id and name
+	public function categories() {
+		return $this->belongsToMany('App\Category')->select('categories.id', 'name');
+	}
 }
