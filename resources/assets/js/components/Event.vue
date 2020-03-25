3089a64b38885238f9f7d60dbfece57c8aa31e95
Sam Li <weisen.li@hotmail.com>
2020-03-24 22:53:28 -0400

    remove event info

--- resources/assets/js/components/Event.vue
+++ resources/assets/js/components/Event.vue
@@ -1,14 +1,13 @@
 <template>
 	<div class="event row" v-on:click="viewEvent(event.id)">
-		<figure class="col-md-3 event__figure">
+		<figure class="col-lg-3 col-md-5 event__figure">
 			<img class="event__thumbnail" :src="event.image" :alt="event.name">
 		</figure>
-		<div class="col-md-9 event__info">
+		<div class="col-lg-9 col-md-7 event__info">
 			<h3 v-html="event.name"></h3>
 			<div><i class="fas fa-calendar-alt"></i>{{event.start_date}}</div>
 			<div><i class="fas fa-map-marker-alt"></i>{{event.location}}</div>
-			<a v-if="event.website" target="_blank" :href="event.website"><i class="fas fa-link"></i>{{event.website}}</a>
-			<div>{{event.address}}</div>
+			<!-- <a v-if="event.website" target="_blank" :href="event.website"><i class="fas fa-link"></i>{{event.website}}</a> -->
 			<Categories v-bind:categories="event.categories" />
 		</div>
 	</div>
