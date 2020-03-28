a5f9c3afeea7b3183040ee07e14284a845666f5f
Sam Li <weisen.li@hotmail.com>
2020-03-27 20:19:26 -0400

    format categorycontroller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,30 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Category;
 
-class CategoryController extends Controller
-{
-    // return list of categories
-    public function index() {
-        return Category::select('id', 'name')->get();
-    }
+class CategoryController extends Controller {
+	// return list of categories
+	public function index() {
+		return Category::select('id', 'name')->get();
+	}
     
-    // return list of events from category
-    public function events($id) {
-        $category = Category::where('id', $id)->first();
-        if ($category) {
-            $events = $category->events()->get();
-            return array(
-                'category' => array(
-                    'id' => $category->id,
-                    'name' => $category->name
-                ),
-                'events' => $events
-            );
-        }
-        else {
-            return array(
-                'msg' => 'No Category Found'
-            );
-        }
-    }
+	// return list of events from category
+	public function events($id) {
+		$category = Category::where('id', $id)->first();
+		if ($category) {
+			$events = $category->events()->get();
+			return array(
+				'category' => array(
+					'id' => $category->id,
+					'name' => $category->name
+				),
+				'events' => $events
+			);
+		}
+		else {
+			return array(
+				'msg' => 'No Category Found'
+			);
+		}
+	}
 }
