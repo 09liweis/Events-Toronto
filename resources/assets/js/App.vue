e1971dbd63c633a3fc903f789bd1ee026a375aa7
Sam Li <weisen.li@hotmail.com>
2020-03-29 14:23:24 -0400

    remove header from vue

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -1,52 +1,7 @@
 <template>
-	<main>
-		<nav id="nav" class="navbar navbar-expand-md navbar-light navbar-laravel">
-			<div class="container">
-				<router-link to="/" class="navbar-brand">Events Toronto</router-link>
-				<router-link to="/dooropen" class="navbar-brand">Door Open Toronto</router-link>
-				<!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
-				<!--    <span class="navbar-toggler-icon"></span>-->
-				<!--</button>-->
-
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<!-- Left Side Of Navbar -->
-					<!--<ul class="navbar-nav mr-auto">-->
-
-					<!--</ul>-->
-
-					<!-- Right Side Of Navbar -->
-					<!--<ul class="navbar-nav ml-auto">-->
-							<!-- Authentication Links -->
-					<!--    @guest-->
-					<!--        <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>-->
-					<!--        <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>-->
-					<!--    @else-->
-					<!--        <li class="nav-item dropdown">-->
-					<!--            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>-->
-					<!--                {{ Auth::user()->name }} <span class="caret"></span>-->
-					<!--            </a>-->
-
-					<!--            <div class="dropdown-menu" aria-labelledby="navbarDropdown">-->
-					<!--                <a class="dropdown-item" href="{{ route('logout') }}"-->
-					<!--                   onclick="event.preventDefault();-->
-					<!--                                 document.getElementById('logout-form').submit();">-->
-					<!--                    {{ __('Logout') }}-->
-					<!--                </a>-->
-
-					<!--                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">-->
-					<!--                    @csrf-->
-					<!--                </form>-->
-					<!--            </div>-->
-					<!--        </li>-->
-					<!--    @endguest-->
-					<!--</ul>-->
-				</div>
-			</div>
-		</nav>
-		<div class="container">
-			<router-view></router-view>
-		</div>
-	</main>
+	<div class="container">
+		<router-view></router-view>
+	</div>
 </template>
 <script>
 export default {};
