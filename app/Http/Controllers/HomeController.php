37832ab16f4e8a96eb67df7e66c41c0d5b9e54a5
Sam Li <weisen.li@hotmail.com>
2020-05-14 20:32:20 -0400

    tab to space homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,25 +5,25 @@ use App\Event;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller {
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct() {
-		// $this->middleware('auth');
-	}
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct() {
+    // $this->middleware('auth');
+  }
 
-	/**
-	 * Show the application dashboard.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function index() {
-		return view('home');
-	}
-	public function detail(int $id) {
-		$event = Event::findOrFail($id);
-		return view('event/detail',['event'=>$event]);
-	}
+  /**
+   * Show the application dashboard.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index() {
+    return view('home');
+  }
+  public function detail(int $id) {
+    $event = Event::findOrFail($id);
+    return view('event/detail',['event'=>$event]);
+  }
 }
