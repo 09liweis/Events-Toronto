f0a2d2d32783543766bc9106f3bd136b3f86d87e
Sam Li <weisen.li@hotmail.com>
2020-05-14 20:31:00 -0400

    tab to space categorycontroller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,27 +7,27 @@ use App\Category;
 
 class CategoryController extends Controller {
 	// return list of categories
-	public function index() {
-		return Category::select('id', 'name')->get();
-	}
+  public function index() {
+    return Category::select('id', 'name')->get();
+  }
     
-	// return list of events from category
-	public function events($id) {
-		$category = Category::where('id', $id)->first();
-		if ($category) {
-			$events = $category->events()->get();
-			return array(
-				'category' => array(
-					'id' => $category->id,
-					'name' => $category->name
-				),
-				'events' => $events
-			);
-		}
-		else {
-			return array(
-				'msg' => 'No Category Found'
-			);
-		}
-	}
+  // return list of events from category
+  public function events($id) {
+    $category = Category::where('id', $id)->first();
+    if ($category) {
+      $events = $category->events()->get();
+      return array(
+        'category' => array(
+          'id' => $category->id,
+          'name' => $category->name
+        ),
+        'events' => $events
+      );
+    }
+    else {
+      return array(
+        'msg' => 'No Category Found'
+      );
+    }
+  }
 }
