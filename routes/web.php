67b351b1ac391d2377ac85affd61434608155469
Sam Li <weisen.li@hotmail.com>
2024-05-14 15:34:08 +0000

    add api events route and controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\EventController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/api/events', [EventController::class, 'index']);
